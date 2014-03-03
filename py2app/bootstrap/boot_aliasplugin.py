898e15e545e57358d84758d50d9de75530228b35
Seanybob <seanybob@gmail.com>
2014-03-02 16:21:01 -0800

    removed bundled dependency py2app

--- py2app/bootstrap/boot_aliasplugin.py
+++
@@ -1,8 +0,0 @@
-def _run(scriptpath):
-    global __file__
-    import os, sys, site
-    sys.frozen = 'macosx_plugin'
-    site.addsitedir(os.environ['RESOURCEPATH'])
-    sys.path.append(os.path.dirname(scriptpath))
-    sys.argv[0] = __file__ = scriptpath
-    execfile(scriptpath, globals(), globals())
